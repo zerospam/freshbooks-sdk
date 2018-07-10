bed344c3cbb8dd4296c60988dd254fc31dfbdcdd
Yan Coutu <yan.coutu@zerospam.ca>
2018-07-10 13:47:29 -0400

    feat(Invoice): Add request to get an invoice share link

---
+++ src/Request/Invoice/ShareLink/GetInvoiceShareLink.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ycoutu
+ * Date: 10/07/18
+ * Time: 11:57 AM
+ */
+
+namespace ZEROSPAM\Freshbooks\Request\Invoice\ShareLink;
+
+use ZEROSPAM\Framework\SDK\Request\Api\BaseRequest;
+use ZEROSPAM\Framework\SDK\Request\Type\RequestType;
+use ZEROSPAM\Framework\SDK\Response\Api\IResponse;
+use ZEROSPAM\Freshbooks\Argument\Invoice\ShareMethodArgument;
+use ZEROSPAM\Freshbooks\Request\HasAccountIdTrait;
+use ZEROSPAM\Freshbooks\Request\IAccountIdRequest;
+use ZEROSPAM\Freshbooks\Response\Invoice\ShareLink\InvoiceShareLinkResponse;
+
+/**
+ * Class GetInvoiceShareLink
+ *
+ * Get the share link of an invoice
+ *
+ * @package ZEROSPAM\Freshbooks\Request\Invoice\ShareLink
+ */
+class GetInvoiceShareLink extends BaseRequest implements IAccountIdRequest
+{
+    use HasAccountIdTrait;
+
+    public function __construct()
+    {
+        $this->addArgument(new ShareMethodArgument('share_link'));
+    }
+
+    /**
+     * Base route without binding.
+     *
+     * @return string
+     */
+    public function baseRoute(): string
+    {
+        return 'accounting/account/:accountId/invoices/invoices/:invoiceId/share_link';
+    }
+
+    /**
+     * Set the id of the invoice
+     *
+     * @param string $id
+     *
+     * @return $this
+     */
+    public function setInvoiceId(string $id): GetInvoiceShareLink
+    {
+        $this->addBinding('invoiceId', $id);
+
+        return $this;
+    }
+
+    /**
+     * Type of request.
+     *
+     * @return RequestType
+     */
+    public function httpType(): RequestType
+    {
+        return RequestType::HTTP_GET();
+    }
+
+    /**
+     * Process the data that is in the response.
+     *
+     * @param array $jsonResponse
+     *
+     * @return IResponse
+     */
+    public function processResponse(array $jsonResponse): IResponse
+    {
+        return new InvoiceShareLinkResponse($jsonResponse['response']['result']['share_link']);
+    }
+}
---
+++ src/Response/Invoice/ShareLink/InvoiceShareLinkResponse.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ycoutu
+ * Date: 10/07/18
+ * Time: 1:37 PM
+ */
+
+namespace ZEROSPAM\Freshbooks\Response\Invoice\ShareLink;
+
+use ZEROSPAM\Framework\SDK\Response\Api\BaseResponse;
+
+/**
+ * Class InvoiceShareLinkResponse
+ *
+ * @property-read string $resourceid
+ * @property-read string $share_method
+ * @property-read string $share_link
+ * @property-read string $resource_type
+ * @property-read int    $clientid
+ *
+ * @package ZEROSPAM\Freshbooks\Response\Invoice\ShareLink
+ */
+class InvoiceShareLinkResponse extends BaseResponse
+{
+
+}
