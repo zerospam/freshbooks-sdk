6253e19730321102908d68d8d15b87966fffebd4
Yan Coutu <yan.coutu@zerospam.ca>
2018-07-10 11:23:40 -0400

    test(Invoice): Add invoice line test

--- tests/src/Requests/Invoice/InvoiceTest.php
+++ tests/src/Requests/Invoice/InvoiceTest.php
@@ -10,6 +10,7 @@ namespace ZEROSPAM\Freshbooks\Test\Requests\Invoice;
 
 use Carbon\Carbon;
 use ZEROSPAM\Framework\SDK\Test\Base\TestCase;
+use ZEROSPAM\Freshbooks\Argument\IncludeArgument;
 use ZEROSPAM\Freshbooks\Request\Invoice\GetInvoiceListRequest;
 use ZEROSPAM\Freshbooks\Request\Invoice\GetInvoiceRequest;
 use ZEROSPAM\Freshbooks\Response\Invoice\InvoiceResponse;
@@ -117,6 +118,7 @@ JSON;
         $this->assertEquals("USD", $response->outstanding->code);
         $this->assertInstanceOf(Carbon::class, $response->due_date);
         $this->assertEquals("2018-08-09", $response->due_date->toDateString());
+        $this->assertNull($response->lines);
     }
 
     public function testGetInvoiceList(): void
@@ -303,5 +305,147 @@ JSON;
         $this->assertEquals("Testing cool", $response[0]->current_organization);
         $this->assertEquals("10% discount: blah blah", $response[1]->terms);
         $this->assertEquals("-2.97", $response[1]->discount_total->amount);
+    }
+
+    public function testGetInvoiceWithLines(): void
+    {
+        $json
+            = <<<JSON
+{
+    "response": {
+        "result": {
+            "invoice": {
+                "province": "Kansas",
+                "deposit_percentage": null,
+                "create_date": "2018-07-10",
+                "outstanding": {
+                    "amount": "11.49",
+                    "code": "USD"
+                },
+                "payment_status": "unpaid",
+                "code": "000000",
+                "lines": [
+                    {
+                        "basecampid": 0,
+                        "qty": "1",
+                        "taxName2": "TVQ",
+                        "modern_time_entries": [],
+                        "taxName1": "TPS",
+                        "taxNumber2": null,
+                        "taxNumber1": null,
+                        "taxAmount2": "9.975",
+                        "taxAmount1": "5",
+                        "type": 0,
+                        "updated": "2018-07-10 11:20:00",
+                        "description": "",
+                        "taskno": 1,
+                        "invoiceid": 129373,
+                        "date": null,
+                        "name": "Inbound filtering 1-5 seats",
+                        "amount": {
+                            "amount": "9.99",
+                            "code": "USD"
+                        },
+                        "unit_cost": {
+                            "amount": "9.99",
+                            "code": "USD"
+                        },
+                        "modern_project_id": null,
+                        "expenseid": 0,
+                        "lineid": 3,
+                        "compounded_tax": false
+                    }
+                ],
+                "ownerid": 1,
+                "vat_number": "",
+                "id": 129373,
+                "gmail": false,
+                "vat_name": null,
+                "v3_status": "sent",
+                "parent": 0,
+                "country": "United States",
+                "lname": "The test",
+                "deposit_status": "none",
+                "estimateid": 0,
+                "ext_archive": 0,
+                "template": "clean-grouped",
+                "basecampid": 0,
+                "generation_date": null,
+                "show_attachments": false,
+                "vis_state": 0,
+                "current_organization": "Testing cool",
+                "status": 2,
+                "due_date": "2018-08-09",
+                "updated": "2018-07-10 08:58:47",
+                "terms": "",
+                "description": "",
+                "discount_description": null,
+                "last_order_status": null,
+                "street2": "",
+                "deposit_amount": null,
+                "paid": {
+                    "amount": "0.00",
+                    "code": "USD"
+                },
+                "invoiceid": 129373,
+                "discount_total": {
+                    "amount": "0.00",
+                    "code": "USD"
+                },
+                "payment_details": "",
+                "address": "",
+                "invoice_number": "0000002",
+                "customerid": 163701,
+                "discount_value": "0",
+                "accounting_systemid": "k0LBE",
+                "return_uri": null,
+                "due_offset_days": 30,
+                "language": "en",
+                "po_number": null,
+                "display_status": "sent",
+                "created_at": "2018-07-10 08:58:46",
+                "autobill_status": null,
+                "date_paid": null,
+                "amount": {
+                    "amount": "11.49",
+                    "code": "USD"
+                },
+                "street": "1 Main Street",
+                "city": "Beach City",
+                "currency_code": "USD",
+                "sentid": 1,
+                "organization": "Testing cool",
+                "dispute_status": null,
+                "fname": "Testing",
+                "notes": "",
+                "auto_bill": false,
+                "accountid": "k0LBE"
+            }
+        }
+    }
+}
+JSON;
+
+        $client  = $this->preSuccess($json);
+        $request = new GetInvoiceRequest();
+        $request->setAccountId('id');
+        $request->setInvoiceId('1324');
+        $request->addArgument(new IncludeArgument('lines'));
+        $client->getOAuthTestClient()->processRequest($request);
+
+        $response = $request->getResponse();
+
+        $this->assertEquals("Kansas", $response->province);
+        $this->assertFalse($response->gmail);
+        $this->assertEquals("11.49", $response->outstanding->amount);
+        $this->assertEquals("USD", $response->outstanding->code);
+        $this->assertInstanceOf(Carbon::class, $response->due_date);
+        $this->assertEquals("2018-08-09", $response->due_date->toDateString());
+        $this->assertCount(1, $response->lines);
+
+        $line = $response->lines[0];
+        $this->assertEquals("9.99", $line->amount->amount);
+        $this->assertEquals("2018-07-10", $line->updated->toDateString());
+        $this->assertEquals("5", $line->taxAmount1);
     }
 }
