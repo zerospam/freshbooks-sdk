ef630e038eb4377b1c937bf0ec3e574af2b6a530
Yan Coutu <yan.coutu@zerospam.ca>
2018-07-13 09:59:30 -0400

    feat(Tax): Add update tax request

---
+++ src/Request/Call/Tax/UpdateTaxRequest.php
@@ -0,0 +1,76 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ycoutu
+ * Date: 13/07/18
+ * Time: 9:56 AM
+ */
+
+namespace ZEROSPAM\Freshbooks\Request\Call\Tax;
+
+use ZEROSPAM\Framework\SDK\Request\Api\BaseRequest;
+use ZEROSPAM\Framework\SDK\Request\Type\RequestType;
+use ZEROSPAM\Framework\SDK\Response\Api\IResponse;
+use ZEROSPAM\Freshbooks\Request\Call\HasAccountIdTrait;
+use ZEROSPAM\Freshbooks\Request\Call\IAccountIdRequest;
+use ZEROSPAM\Freshbooks\Request\Data\Tax\TaxData;
+use ZEROSPAM\Freshbooks\Response\Tax\TaxResponse;
+
+class UpdateTaxRequest extends BaseRequest implements IAccountIdRequest
+{
+    use HasAccountIdTrait;
+
+    /** @var TaxData */
+    private $tax;
+
+    public function __construct(TaxData $data)
+    {
+        $this->tax = $data;
+    }
+
+    /**
+     * Type of request.
+     *
+     * @return RequestType
+     */
+    public function httpType(): RequestType
+    {
+        return RequestType::HTTP_POST();
+    }
+
+    /**
+     * Process the data that is in the response.
+     *
+     * @param array $jsonResponse
+     *
+     * @return IResponse
+     */
+    public function processResponse(array $jsonResponse): IResponse
+    {
+        return new TaxResponse($jsonResponse["response"]["result"]["tax"]);
+    }
+
+    /**
+     * Base route without binding.
+     *
+     * @return string
+     */
+    public function baseRoute(): string
+    {
+        return 'accounting/account/:accountId/taxes/taxes/:taxId';
+    }
+
+
+    /**
+     * Set the tax id
+     *
+     * @param string $id
+     * @return $this
+     */
+    public function setTaxId(string $id): UpdateTaxRequest
+    {
+        $this->addBinding('taxId', $id);
+
+        return $this;
+    }
+}
