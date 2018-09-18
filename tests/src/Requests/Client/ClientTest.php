e2f04ba2f92a9f0cc789add60390d432d7ed4ded
Yan Coutu <yan.coutu@zerospam.ca>
2018-09-18 09:44:37 -0400

    test(LatePaymentReminder): Add tests for late payment reminders

--- tests/src/Requests/Client/ClientTest.php
+++ tests/src/Requests/Client/ClientTest.php
@@ -8,6 +8,7 @@
 
 namespace ZEROSPAM\Freshbooks\Test\Requests\Client;
 
+use Carbon\Carbon;
 use ZEROSPAM\Framework\SDK\Response\Api\EmptyResponse;
 use ZEROSPAM\Framework\SDK\Test\Base\TestCase;
 use ZEROSPAM\Freshbooks\Request\Call\Clients\ClientReadRequest;
@@ -16,6 +17,7 @@ use ZEROSPAM\Freshbooks\Request\Call\Clients\Collection\ClientCreateRequest;
 use ZEROSPAM\Freshbooks\Request\Call\Clients\ClientDeleteRequest;
 use ZEROSPAM\Freshbooks\Request\Call\Clients\ClientUpdateRequest;
 use ZEROSPAM\Freshbooks\Request\Data\Client\ClientData;
+use ZEROSPAM\Freshbooks\Request\Data\Client\LatePayment\ReminderData;
 use ZEROSPAM\Freshbooks\Response\Clients\ClientResponse;
 
 class ClientTest extends TestCase
@@ -100,7 +102,13 @@ JSON;
         "s_street2": "App. 6",
         "username": "john.doe",
         "vat_name": "TAX",
-        "vat_number": "12345"
+        "vat_number": "12345",
+        "late_reminders": [{
+            "body": "Please pay ASAP",
+            "delay": -3,
+            "enabled": true,
+            "position": 1
+        }]
     }
 }
 JSON;
@@ -155,13 +163,31 @@ JSON;
                 "s_code": "142857",
                 "organization": "Company Inc.",
                 "p_country": "Billand",
-                "currency_code": "USD"
+                "currency_code": "USD",
+                "late_reminders": [{
+                    "body": "Please pay ASAP",
+                    "delay": -3,
+                    "enabled": true,
+                    "position": 1,
+                    "userid": 12345,
+                    "created_at": "2018-09-17 8:47:00",
+                    "updated_at": null
+                }]
             }
         }
     }
 }
 JSON;
-        $client       = $this->preSuccess($jsonResponse);
+
+        $client = $this->preSuccess($jsonResponse);
+
+        $reminders = [
+            (new ReminderData)
+            ->setBody("Please pay ASAP")
+            ->setDelay(-3)
+            ->setEnabled(true)
+            ->setPosition(1)
+        ];
 
         $clientData = (new ClientData)
             ->setAllowLateFees(true)
@@ -196,7 +222,8 @@ JSON;
             ->setSStreet2("App. 6")
             ->setUsername("john.doe")
             ->setVatName("TAX")
-            ->setVatNumber("12345");
+            ->setVatNumber("12345")
+            ->setLateReminders($reminders);
 
         $request = (new ClientCreateRequest($clientData))
             ->setAccountId('abcde');
@@ -210,6 +237,18 @@ JSON;
         $this->assertEquals("abcde", $response->accounting_systemid);
         $this->assertEquals("Shipville", $response->s_city);
         $this->assertFalse($response->pref_gmail);
+
+        $lateReminders = $response->late_reminders;
+        $lateReminder = $lateReminders[0];
+
+        $this->assertEquals(1, count($lateReminders));
+
+        $this->assertEquals("Please pay ASAP", $lateReminder->body);
+        $this->assertEquals(-3, $lateReminder->delay);
+        $this->assertTrue($lateReminder->enabled);
+        $this->assertEquals(1, $lateReminder->position);
+        $this->assertTrue($lateReminder->created_at->equalTo(Carbon::createFromFormat('Y-m-d H:i', '2018-09-17 08:47')));
+        $this->assertNull($lateReminder->updated_at);
     }
 
     public function testUpdateClient(): void
@@ -249,7 +288,20 @@ JSON;
         "s_street2": "App. 6",
         "username": "john.doe",
         "vat_name": "TAX",
-        "vat_number": "12345"
+        "vat_number": "12345",
+        "late_reminders": [{
+            "position": 2,
+            "enabled": false,
+            "body": null,
+            "delay": 4,
+            "id": "userid5545"
+        }, {
+            "position": 1,
+            "enabled": true,
+            "body": "Last day",
+            "delay": -1,
+            "id": "userid5544"
+        }]
     }
 }
 JSON;
@@ -304,13 +356,45 @@ JSON;
                 "s_code": "142857",
                 "organization": "Company Inc.",
                 "p_country": "Billand",
-                "currency_code": "USD"
+                "currency_code": "USD",
+                "late_reminders": [{
+                    "position": 2,
+                    "enabled": false,
+                    "body": null,
+                    "delay": 4,
+                    "userid": 5545,
+                    "created_at": "2018-02-02 11:00:00",
+                    "updated_at": "2018-09-01 12:00:00"
+                }, {
+                    "position": 1,
+                    "enabled": true,
+                    "body": "Last day",
+                    "delay": -1,
+                    "userid": 5544,
+                    "created_at": "2018-01-01 10:00:00",
+                    "updated_at": "2018-09-01 12:00:00"
+                }]
             }
         }
     }
 }
 JSON;
         $client  = $this->preSuccess($jsonResponse);
+
+        $reminders = [
+            (new ReminderData)
+                ->setPosition(2)
+                ->setEnabled(false)
+                ->setDelay(4)
+                ->setBody(null)
+                ->setUserid(5545),
+            (new ReminderData)
+                ->setPosition(1)
+                ->setEnabled(true)
+                ->setDelay(-1)
+                ->setBody("Last day")
+                ->setUserid(5544),
+        ];
 
         $clientData = (new ClientData)
             ->setAllowLateFees(true)
@@ -345,7 +429,8 @@ JSON;
             ->setSStreet2("App. 6")
             ->setUsername("john.doe")
             ->setVatName("TAX")
-            ->setVatNumber("12345");
+            ->setVatNumber("12345")
+            ->setLateReminders($reminders);
 
         $request = (new ClientUpdateRequest($clientData))
             ->setAccountId('abcde')
@@ -357,9 +442,22 @@ JSON;
 
         $this->validateRequest($client, $jsonRequest);
 
+        $lateReminders = $response->late_reminders;
+
         $this->assertEquals("abcde", $response->accounting_systemid);
         $this->assertEquals("Shipville", $response->s_city);
         $this->assertFalse($response->pref_gmail);
+
+        $this->assertEquals(2, count($lateReminders));
+
+        $lateReminder = $lateReminders[0];
+
+        $this->assertNull($lateReminder->body);
+        $this->assertEquals(4, $lateReminder->delay);
+        $this->assertFalse($lateReminder->enabled);
+        $this->assertEquals(2, $lateReminder->position);
+        $this->assertTrue($lateReminder->created_at->equalTo(Carbon::createFromFormat('Y-m-d H:i', '2018-02-02 11:00')));
+        $this->assertTrue($lateReminder->updated_at->equalTo(Carbon::createFromFormat('Y-m-d H:i', '2018-09-01 12:00')));
     }
 
     public function testDeleteClient(): void
